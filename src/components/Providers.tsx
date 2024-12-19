300dd3cece3cf4b2569a183c173942058a418eb1
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-12-19 23:13:15 +0530

    Project Setup

---
+++ src/components/Providers.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
