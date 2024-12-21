9f895215657042f34c505f696f957a20342fb7b8
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-12-21 23:22:16 +0530

    Add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import TopNav from "@/components/navbar/TopNav";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">Hello</h1>
+      <TopNav />
     </div>
   );
 }
