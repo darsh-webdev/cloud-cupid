24a5b4d149a8a45db205f16711eaef200c7ac6af
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-12-22 20:42:33 +0530

    Add Login and Register form UI with validation

---
+++ src/lib/schemas/RegisterSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z.string().min(4),
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
