eec48a6cfc33f030b9d8920ac3cbaaa4f8e25f0e
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-12-31 18:23:18 +0530

    Add Profile edit route and page

---
+++ src/app/members/edit/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { CardHeader, Divider, CardBody } from "@nextui-org/react";
+
+const MemberEditPage = () => {
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-default">
+        Edit Profile
+      </CardHeader>
+      <Divider />
+      <CardBody>Edit page</CardBody>
+    </>
+  );
+};
+
+export default MemberEditPage;
